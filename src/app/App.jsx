f5f892c0b3f2e05fd4301b9b675a2b39f2a6f33a
RAYHunts <rayhunts710@gmail.com>
2023-10-30 09:23:47 +0700

    separate question to files

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,66 +1,7 @@
 import { useEffect, useState } from "react";
-
+import questions from "../constants/questions";
 const App = () => {
-    const questions = [
-        {
-            question: "What type of smartphone do you use?",
-            choices: ["Android", "iOS", "Windows"],
-            name: "os",
-        },
-        {
-            question: "How often do you update your computer's operating system or software?",
-            choices: [
-                "Regularly (automatic updates)",
-                "Occasionally (manual updates)",
-                "Rarely (never update)",
-            ],
-            name: "update",
-        },
-        {
-            question: "Which social media platform do you use most frequently?",
-            choices: ["Facebook", "Instagram", "Twitter"],
-            name: "social",
-        },
-        {
-            question: "How do you primarily access the internet at home?",
-            choices: ["Wi-Fi", "Ethernet", "Mobile data"],
-            name: "internet",
-        },
-        {
-            question: "What is your preferred web browser?",
-            choices: ["Google Chrome", "Mozilla Firefox", "Microsoft Edge"],
-            name: "browser",
-        },
-        {
-            question: "Do you use a virtual private network (VPN) for online privacy and security?",
-            choices: ["Yes, regularly", "Occasionally", "No"],
-            name: "vpn",
-        },
-        {
-            question: "Which streaming service do you use for entertainment?",
-            choices: ["Netflix", "Amazon Prime Video", "Disney+"],
-            name: "streaming",
-        },
-        {
-            question: "How do you store your important digital files and documents?",
-            choices: [
-                "Cloud storage (e.g., Google Drive, Dropbox)",
-                "External hard drive",
-                "On my computer's local storage",
-            ],
-            name: "storage",
-        },
-        {
-            question: "Have you ever used a smart home device (e.g., smart thermostat, voice assistant)?",
-            choices: ["Yes, multiple", "Yes, one", "No, never"],
-            name: "smart",
-        },
-        {
-            question: "How often do you participate in online gaming or e-sports?",
-            choices: ["Daily", "Weekly", "Rarely or never"],
-            name: "gaming",
-        },
-    ];
+    
 
     const [step, setStep] = useState(parseInt(localStorage.getItem("step"), 10) || 1);
     const [formData, setFormData] = useState(JSON.parse(localStorage.getItem("formData")) || {});
@@ -123,7 +64,7 @@ const App = () => {
 
     return (
         <main className="h-screen flex justify-center items-center p-2 bg-gradient-to-b from-white via-red-400 to-primary font-jakarta">
-            <div className="p-6 bg-white/25 backdrop-blur-md rounded-md relative max-w-[480px] container">
+            <div className="p-6 bg-white/25 backdrop-blur-md rounded-md relative max-w-lg container">
 
                 {!isSubmitted && (
                     <>
---
+++ src/constants/questions.ts
@@ -0,0 +1,62 @@
+const questions = [
+    {
+        question: "What type of smartphone do you use?",
+        choices: ["Android", "iOS", "Windows"],
+        name: "os",
+    },
+    {
+        question: "How often do you update your computer's operating system or software?",
+        choices: [
+            "Regularly (automatic updates)",
+            "Occasionally (manual updates)",
+            "Rarely (never update)",
+        ],
+        name: "update",
+    },
+    {
+        question: "Which social media platform do you use most frequently?",
+        choices: ["Facebook", "Instagram", "Twitter"],
+        name: "social",
+    },
+    {
+        question: "How do you primarily access the internet at home?",
+        choices: ["Wi-Fi", "Ethernet", "Mobile data"],
+        name: "internet",
+    },
+    {
+        question: "What is your preferred web browser?",
+        choices: ["Google Chrome", "Mozilla Firefox", "Microsoft Edge"],
+        name: "browser",
+    },
+    {
+        question: "Do you use a virtual private network (VPN) for online privacy and security?",
+        choices: ["Yes, regularly", "Occasionally", "No"],
+        name: "vpn",
+    },
+    {
+        question: "Which streaming service do you use for entertainment?",
+        choices: ["Netflix", "Amazon Prime Video", "Disney+"],
+        name: "streaming",
+    },
+    {
+        question: "How do you store your important digital files and documents?",
+        choices: [
+            "Cloud storage (e.g., Google Drive, Dropbox)",
+            "External hard drive",
+            "On my computer's local storage",
+        ],
+        name: "storage",
+    },
+    {
+        question: "Have you ever used a smart home device (e.g., smart thermostat, voice assistant)?",
+        choices: ["Yes, multiple", "Yes, one", "No, never"],
+        name: "smart",
+    },
+    {
+        question: "How often do you participate in online gaming or e-sports?",
+        choices: ["Daily", "Weekly", "Rarely or never"],
+        name: "gaming",
+    },
+];
+
+export default questions
